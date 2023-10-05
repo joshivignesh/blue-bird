1b2c07740a8c5987034d7111c13c32a7031b44d9
Vignesh <joshivignesh@users.noreply.github.com>
2023-10-05 13:36:44 +0530

    Added Client button, Server button, redirect URLs and Middleware

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import AuthButtonClient from "@/auth-button-client";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Login() {
+    const supabase = createServerComponentClient({ cookies })
+const {data:{session},} = await supabase.auth.getSession();
+
+if(session){
+    redirect("/");
+}
+
+return <AuthButtonClient session={session} />
+
+}
